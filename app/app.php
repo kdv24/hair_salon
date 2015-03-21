6c8e8fa831f5b0043fbfe7ffadc3cf69c33e08ec
Kelly de Vries <kdv@me.com>
2015-03-21 13:14:52 -0700

    Add initial twig files for home page and edit page for stylists. Add ->get for home page.

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
 	require_once __DIR__."/../vendor/autoload.php";
-	require_once __DIR__."/src/Stylist.php";
+	require_once __DIR__."/../src/Stylist.php";
 
 	$app = new Silex\Application();
 	$app['debug']=true;
@@ -8,6 +8,16 @@
 	$DB= new PDO('pgsql:host=localhost; dbname=hair_salon');
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+	$app->get("/", function () use ($app)
+	{
+		return $app['twig']->render('stylists.twig', array('stylists' => Stylist::getAll()));
+	});
+
 
 
 	return $app;
