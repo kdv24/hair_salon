c92aec0e73adcea59162b69afd6265da6ead64e0
Kelly de Vries <kdv@me.com>
2015-03-23 16:18:08 -0700

    Add tests and methods for Client. Pass tests

--- src/Client.php
+++ src/Client.php
@@ -5,7 +5,7 @@
 		private $id;
 		private $stylist_id;
 
-		function __construct($name, $id=null, $stylist_id=null)
+		function __construct($name, $id=null, $stylist_id)
 		{
 			$this->name = $name;
 			$this->id = $id;
@@ -32,21 +32,46 @@
 			$this->id = (int)$new_id;
 		}
 
-		function getCategoryId()
+		function getStylistId()
 		{
 			return $this->stylist_id;
 		}
 
-		function setCategoryId($new_stylist_id)
+		function setStylistId($stylist_id)
 		{
-			$this->stylist_id = $new_stylist_id;
+			$this->stylist_id = (int) $stylist_id;
 		}
 
 		function save()
 		{
-			$statement= $GLOBALS['DB']->exec("INSERT INTO clients (client_name) VALUES ('{$this->getName()}') RETURNING id;");
+			$statement= $GLOBALS['DB']->query("INSERT INTO clients (client_name, stylist_id) VALUES ('{$this->getName()}', {$this->getStylistId()}) RETURNING id;");
 			$result = $statement->fetch(PDO::FETCH_ASSOC);
 			$this->setId($result['id']);
+		}
+
+		function update($new_client_name)
+		{
+			$GLOBALS['DB']->exec("UPDATE clients SET client_name = '{$new_client_name}' WHERE id = {$this->getId()};");
+			$this->setName($new_client_name);
+		}
+
+		static function getAll()
+		{
+			$all_clients = $GLOBALS['DB']->query("SELECT * FROM clients;");
+			$clients_to_return = array();
+			foreach($all_clients as $current_client) {
+				$name = $current_client['client_name'];
+				$id = $current_client['id'];
+				$stylist_id = $current_client['stylist_id'];
+				$new_client = new Client($name, $id, $stylist_id);
+				array_push($clients_to_return, $new_client);
+			}	
+			return $clients_to_return;
+		}
+
+		static function deleteAll()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM clients *;");
 		}
 	}
 ?>	
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,16 +5,18 @@
 	* @backupStaticAttributes disabled
 	*/
 
-require_once "src/Client.php";	
+	require_once "src/Client.php";	
+	require_once "src/Stylist.php";
 
-$DB = new PDO('pgsql:host=localhost; dbname=hair_salon_test');
+	$DB = new PDO('pgsql:host=localhost; dbname=hair_salon_test');
 
-class ClientTest extends PHPUnit_Framework_TestCase
-{
-	// protected function tearDown()
-	// {
-	// 	Client::deleteAll();
-	// }
+	class ClientTest extends PHPUnit_Framework_TestCase
+	{
+
+	protected function tearDown()
+	{
+		Client::deleteAll();
+	}
 
 	function test_getName()
 	{
@@ -53,72 +55,78 @@ class ClientTest extends PHPUnit_Framework_TestCase
 		$name = "Joe";
 		$id = 1;
 		$stylist_id = 2;
-		$test_stylist = new Stylist($name, $id, $stylist_id);
+		$test_client = new Client($name, $id, $stylist_id);
 
 		//Act
-		$test_stylist->save();
+		$test_client->save();
+
+		//Assert
+		$result= Client::getAll();
+		var_dump($result);
+		$this->assertEquals($test_client, $result[0]);
+	}
+
+	function test_update()
+	{
+		//Arrange
+		$name = "Susanna";
+		$id = 1;
+		$stylist_id = 2;
+		$test_name = new Client($name, $id, $stylist_id);
+		$test_name->save();
+		$new_name= "Linda";
+
+		//Act
+		$test_name->update($new_name);
+
+		//Assert
+		$this->assertEquals("Linda", $test_name->getName());
+	}
+
+
+	function test_getAll()
+	{
+		//Arrange
+		$name = "Joe";
+		$id= 1;
+		$stylist_id = 10;
+		$test_client = new Client($name, $id, $stylist_id);
+
+		$name2 = "George";
+		$id2 = 2;
+		$stylist_id2 = 20;
+		$test_client2 = new Client($name2, $id2, $stylist_id2);
+
+		//Act
+		$test_client->save();
+		$test_client2->save();
+
+		//Assert
+		$result= Client::getAll();
+		$this->assertEquals([$test_client, $test_client2], $result);
+	}
+
+	function test_deleteAll()
+	{
+		//Arrange
+		$name = "Susanna";
+		$id= 1;
+		$stylist_id = 10;
+		$test_client = new Client($name, $id,  $stylist_id);
+
+		$name2 = "Staci";
+		$id2 = 2;
+		$stylist_id2 = 20;
+		$test_client2 = new Client($name2, $id2, $stylist_id2);
+
+		//Act
+		$test_client->save();
+		$test_client2->save();
+		Client::deleteAll();
 
 		//Assert
 		$result= Stylist::getAll();
-		$this->assertEquals($test_stylist, $result[0]);
+		$this->assertEquals([], $result);
 	}
-
-	// function test_update()
-	// {
-	// 	//Arrange
-	// 	$name = "Susanna";
-	// 	$id = 1;
-	// 	$test_name = new Stylist($name, $id);
-	// 	$test_name->save();
-	// 	$new_name= "Linda";
-
-	// 	//Act
-	// 	$test_name->update($new_name);
-
-	// 	//Assert
-	// 	$this->assertEquals("Linda", $test_name->getName());
-	// }
-
-
-	// function test_getAll()
-	// {
-	// 	//Arrange
-	// 	$name = "Susanna";
-	// 	$id= 1;
-	// 	$test_Stylist = new Stylist($name, $id);
-
-	// 	$name2 = "Staci";
-	// 	$id2 = 2;
-	// 	$test_Stylist2 = new Stylist($name2, $id2);
-
-	// 	//Act
-	// 	$test_Stylist->save();
-	// 	$test_Stylist2->save();
-
-	// 	//Assert
-	// 	$result= Stylist::getAll();
-	// 	$this->assertEquals([$test_Stylist, $test_Stylist2], $result);
-	// }
-
-	// function test_deleteAll()
-	// {
-	// 	//Arrange
-	// 	$name = "Susanna";
-	// 	$id= 1;
-	// 	$test_Stylist = new Stylist($name, $id);
-
-	// 	$name2 = "Staci";
-	// 	$id2 = 2;
-	// 	$test_Stylist2 = new Stylist($name2, $id2);
-
-	// 	//Act
-	// 	$test_Stylist->save();
-	// 	$test_Stylist2->save();
-	// 	Stylist::deleteAll();
-
-	// 	//Assert
-	// 	$result= Stylist::getAll();
-	// 	$this->assertEquals([], $result);
-	// }
 }
 ?>
